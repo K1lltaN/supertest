f500dc6f10a0ca1c6c9b5cb668080b15d81c3cda
Максим Синявський <killtan@Air-KilltaN.fritz.box>
2023-01-31 12:32:05 +0100

    supertest

---
+++ script.js
@@ -0,0 +1,11 @@
+class Cat {
+    constructor (age, color, type) {
+        this.age = age;
+        this.color = color;
+        this.type = type;
+    }
+}
+let firstCat = new Cat (2, 'White', 'nemetz');
+console.log(firstCat);
+let secindCat = new Cat(1, 'Black', 'ukrainetz');
+console.log(secindCat);
